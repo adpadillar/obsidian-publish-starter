ef40f8900bf59a2d51a626b2ae06d90dff63711b
Axel Padilla <adpadillar25@gmail.com>
2023-07-24 14:52:28 -0700

    full w in small screens

--- components/misc/note-preview.tsx
+++ components/misc/note-preview.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const NotePreview = ({ title, content }: Props) => {
   return (
-    <span className="note-preview block col-span-2 max-w-[400px] rounded shadow-sm p-5 bg-white cursor-pointer text-lg hover:bg-gray-200 hover:border-transparent">
+    <span className="note-preview block col-span-2 rounded shadow-sm p-5 bg-white cursor-pointer text-lg hover:bg-gray-50 hover:border-transparent">
       <span className="block font-bold leading-snug tracking-tight truncate mb-1">
         {title}
       </span>
