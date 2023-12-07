d0dc8ee9a9247ebf1f14633e030e42c6de6918e3
Axel Padilla <adpadillar25@gmail.com>
2023-12-06 20:59:28 -0600

    posthog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,41 @@ import { AppProps } from "next/app";
 import "../styles/index.css";
 import { DefaultSeo } from "next-seo";
 import SEO from "../next-seo.config";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
     <>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
+      <PostHogProvider client={posthog}>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </PostHogProvider>
     </>
   );
 }
